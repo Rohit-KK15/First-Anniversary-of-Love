a204005349affc5ce1b4fabf5c1d7aeb98237da8
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2024-08-23 02:16:00 +0530

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
 let text1 = "Oy... Bangarammm!!";
-let text2 = "Today is the day I first met you, bangaram. It feels like magic happened that day, and since then, my world has been so much better just because you’re in it."
+let text2 = "Today is the day I first met you. It feels like magic happened that day, and since then, my world has been so much better just because you’re in it."
 let speed = 1000;
 
 function typeWriter(text, para){
